aa6d6c488dda49ff8b67176cd399d2d1fb9a40bf
Ume495 <umekalsoomfatima495@gmail.com>
2024-06-03 16:04:23 +0500

    files added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Todo from "./Todo";
+import Header from "./components/Header";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
